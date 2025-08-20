f9706acd5e39c9e4729ef3977088982b78194b27
skar777123 <sanskargaikwad206@gmail.com>
2025-08-20 21:37:48 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,89 @@
-import Image from "next/image";
+"use client";
+import { Bonheur_Royale } from "next/font/google";
+import { useState } from "react";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+
+const br = Bonheur_Royale({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Home() {
+  const notify = () => toast("Absoultely right...");
+  const [q, setQ] = useState("");
+  const [i, setI] = useState(0);
+  const clue = [
+    { question: "Find what Ganesha Wrote?", answer: "siddhinath" },
+    { question: "Find the Elephant?", answer: "gajanana" },
+    { question: "Find the who gives solu􀆟ons?", answer: "vighnahartha" },
+    { question: "How Ganpa􀆟 was born?", answer: "bhalchandra" },
+    { question: "Everything ends at me?", answer: "dhumravarna" },
+  ];
+  const handleSubmit = () => {
+    if (q.toLowerCase() === clue[i].answer) {
+      setI(i + 1);
+      notify();
+    }
+  };
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className=" h-screen w-screen">
+      <main className="flex-col h-screen w-screen">
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex justify-center items-center bg-cover">
+          <img
+            className="bg-cover"
+            src="/g-logo.png"
+            alt="logo"
+            width={150}
+            height={100}
+          />
         </div>
+        <div className="text-center">
+          <h1 className={` font-mono  text-orange-400 text-4xl`}>
+            Techkshetra (Tech Club) - Technovinayak
+          </h1>
+          <h1 className={`font-mono font-bold text-orange-400 text-5xl`}>
+            Modak Hunt
+          </h1>
+        </div>
+        <>
+          <div className="text-center mt-20">
+            <h1 className={`${br.className}  text-orange-400 text-7xl`}>
+              {clue[i].question}
+            </h1>
+            <h1 className={`font-mono  text-orange-400 text-2xl`}>
+              (complete the task and enter the code!)
+            </h1>
+          </div>
+          <div className="text-center mt-16">
+            <input
+              type="text"
+              placeholder="Enter the Code"
+              className={`font-mono  bg-amber-500 p-2 rounded-2xl text-2xl text-white text-center border-amber-400`}
+              onChange={(e) => {
+                setQ(e.target.value);
+              }}
+            />
+            <input
+              type="submit"
+              onClick={handleSubmit}
+              className={`font-mono  cursor-pointer bg-amber-500 p-2 rounded-2xl text-2xl ml-2 text-white text-center border-amber-400`}
+            />
+          </div>
+        </>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
